5510ec2bef2ab228749cc3393c536ad65baaef22
Evan Graham <evangraham19@gmail.com>
2023-09-04 16:49:24 -0700

    connecting seeders

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const methodOverride= require('method-override')
 const app = express()
-
+const mongoose= require('mongoose')
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -10,6 +10,18 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use('/places', require('./controllers/places'))
+
+
+
+
+async function main(){
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Mongodb connected")
+}
+main().catch(err => console.log(err));
+
+
 
 app.get('/', (req, res) => {
     res.render('home')
